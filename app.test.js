bd780713da3d8648599b6be1ddd195a18b9e5a8f
jeromedecoster <jeromedecoster@users.noreply.github.com>
2020-04-03 14:32:35 +0200

    better

--- app.test.js
+++ app.test.js
@@ -7,7 +7,7 @@ describe('/add', () => {
     it('success', (done) => {
         const a = 2
         const b = 4
-        // a comment
+        // a better comment
         const expected = 6
 
         request(app)
