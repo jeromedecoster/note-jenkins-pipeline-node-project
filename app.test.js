949424ebe860437ac8a731e9c66dc2ca45b009d5
jeromedecoster <jeromedecoster@users.noreply.github.com>
2020-04-03 15:51:56 +0200

    6

--- app.test.js
+++ app.test.js
@@ -7,8 +7,8 @@ describe('/add', () => {
     it('success', (done) => {
         const a = 2
         const b = 4
-        // final test !
-        const expected = 7
+        // final test
+        const expected = 6
 
         request(app)
             .post('/add')
